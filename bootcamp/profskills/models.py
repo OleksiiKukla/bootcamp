441466f4f34cc8f03ce84b2c6190716fc7fd855d
o.kukla <o.kukla@andersenlab.com>
2024-02-26 17:34:26 +0200

    initial commit and postgres connection set-up

---
+++ bootcamp/profskills/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Professions(models.Model):
+    pass
+
+class Skill(models.Model):
+    pass
+
+class Topic(models.Model):
+    pass
